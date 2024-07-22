db031a0eaac7d2934bd9e4e7d293148b464caedf
“Laura <lp244@duke.edugit config --global user.name Laura Penggit config --global user.email lp244@duke.edu>
2024-07-22 02:07:13 -0400

    changed again

--- start.py
+++ start.py
@@ -129,12 +129,12 @@ class ThemeManager:
         #CLEARS APP ---------------------------------------
         if clear_resp == 'yes':
             # Specify the directory path
-            #directory = 'llaurapeng/viz-automation/main/pages'
+            directory = 'llaurapeng/viz-automation/main/pages'
 
-            curr_dir = os.getcwd()
+            #curr_dir = os.getcwd()
 
             # Specify the new file name and path
-            directory = curr_dir + '/pages'
+            #directory = curr_dir + '/pages'
 
             # Iterate over all files in the directory
             for filename in os.listdir(directory):
